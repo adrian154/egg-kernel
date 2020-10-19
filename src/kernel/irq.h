1a07498b5bd168a38ca863932dc6c9e3fa1a8bfa
adrian154 <94568a@gmail.com>
2020-10-19 16:41:08 -0700

    refactor irq code

--- src/kernel/irq.h
+++ src/kernel/irq.h
@@ -2,15 +2,32 @@
 #define __IRQ_H
 
 #include "kernel.h"
+#include "interrupt.h"
 
 // Data on the stack before C IRQ handler starts working
 // Resembles ExceptionFrame (see exception.h) but lacks `errorCode` since IRQ handlers don't push error codes
 struct IRQFrame {
-    uint32_t DS, ES, FS, GS;                                        // Also pushed
-    uint32_t EDI, ESI, kernelEBP, kernelESP, EBX, EDX, ECX, EAX;    // Pushed by PUSHA in common exception handler 
-    uint32_t interruptNumber;                                       // Pushed by exception handlers
-    uint32_t EFLAGS, CS, EIP;                                       // Pushed by processor automagically
-    uint32_t intSS, intESP;                                         // Might be pushed if the interrupted code was usermode; only access if you know this for certain! 
+
+    // Pushed by commonIRQHandler
+    // See exception.h/exception.asm for similar code with better explanations
+    uint32_t GS;
+    uint32_t FS;
+    uint32_t ES;
+    uint32_t DS;
+    uint32_t EDI;
+    uint32_t ESI;
+    uint32_t EBP;
+    uint32_t ESP;
+    uint32_t EBX;
+    uint32_t EDX;
+    uint32_t ECX;
+    uint32_t EAX;
+
+    // Pushed by each exception handler
+    uint32_t interruptNumber;
+
+    struct InterruptFrame interruptFrame;
+
 }__attribute__((packed));
 
 // Typedef function pointer since function pointers in C are abhorrent
